57fb029709b2cfb5ae09e27a27a27a802b258d23
Julia Sa Eller <juliasaeller@40o.fct.unesp.br>
2024-10-04 10:45:48 -0300

    alteracoes para atividade com branch

--- SegundaAtividade.c
+++ SegundaAtividade.c
@@ -12,17 +12,46 @@ double divisao(char* a, char* b){
     return aFloat/bFloat;
 }
 
+int adicao(double a, double b){
+    double soma;
+    soma = a + b;
+    return soma;
+}
+
+double subtracao(double a, double b){
+    double sub;
+    sub = a - b;
+    return sub;
+}
+
+double potenciacao(double a, double b){
+    double potencia;
+    potencia = pow(a, b);
+    return ;
+}
+
+double raiz_quadrada(int a){
+    double raiz;
+    raiz = sqrt(a);
+    return raiz;
+}
+
+
 
 int main(){
     double a, b;
     char n[20], m[20];
 
-    printf("\nMULTIPLICACAO: ");
+
+
+    
+
+    /*printf("\nMULTIPLICACAO: ");
     printf("\nDigite um numero: ");
     scanf("%lf", &a);
     printf("\nDigite outro numero: ");
     scanf("%lf", &b);
-    printf("\n%.2f x %.2f = %.2f", a, b, multiplicacao(a, b));
+    printf("\n%.2f x %.2f = %.2f\n", a, b, multiplicacao(a, b));
 
 
     printf("\nDIVISAO: ");
@@ -31,7 +60,7 @@ int main(){
     getchar();
     printf("\nDigite outro numero: ");
     scanf("%s", m);
-    printf("\n%s / %s = %.2f", n, m, divisao(n, m));
+    printf("\n%s / %s = %.2f", n, m, divisao(n, m));*/
 
 
     return 0;
