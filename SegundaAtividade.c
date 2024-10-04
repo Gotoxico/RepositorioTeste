b0c3b9b79a9dad84b7b3680e238357a199b55453
Rodrigo Goto <bazingaf2004@outlook.com>
2024-10-04 10:33:29 -0300

    Adicionado divisao na main

--- SegundaAtividade.c
+++ SegundaAtividade.c
@@ -15,7 +15,7 @@ double divisao(char* a, char* b){
 
 int main(){
     double a, b;
-    char n, m;
+    char n[20], m[20];
 
     printf("\nMULTIPLICACAO: ");
     printf("\nDigite um numero: ");
@@ -24,14 +24,14 @@ int main(){
     scanf("%lf", &b);
     printf("\n%.2f x %.2f = %.2f", a, b, multiplicacao(a, b));
 
-/*
+
     printf("\nDIVISAO: ");
     printf("\nDigite um numero: ");
-    scanf("%c");
+    scanf("%s", n);
+    getchar();
     printf("\nDigite outro numero: ");
-    scanf("%lf");
-    printf("\n%.2f / %.2f = %.2f ");
-*/
+    scanf("%s", m);
+    printf("\n%s / %s = %.2f", n, m, divisao(n, m));
 
 
     return 0;
