0697fb6ffacd78008521aa1f40aa9ed52fe84cd4
Rodrigo Goto <bazingaf2004@outlook.com>
2024-10-04 10:11:48 -0300

    adicionado divisao

--- SegundaAtividade.c
+++ SegundaAtividade.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+#include <stdlib.h>
 
 double multiplicacao(double a, double b){
     double mult;
     mult = a * b;
     return mult;
+}
+
+double divisao(char* a, char* b){
+    double aFloat = atof(a), bFloat = atof(b);
+    return aFloat/bFloat;
 }
 
 
