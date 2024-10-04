5a4b02f7eaa3b036da4087ac8b0fd5ea9ed98d6a
Rodrigo Goto <bazingaf2004@outlook.com>
2024-10-04 09:18:04 -0300

    Adicionado funcao calculadora

---
+++ Calculadora.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+float soma(char* valor1, char* valor2){
+    float value1 = atof(valor1), value2 = atof(valor2);
+    return value1 + value2;
+}
