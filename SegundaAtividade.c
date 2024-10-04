be69da630278f8ba278aa883e7e82ace172136c1
Julia Sa Eller <juliasaeller@40o.fct.unesp.br>
2024-10-04 10:19:29 -0300

    chamada da multiplicacao na main

--- SegundaAtividade.c
+++ SegundaAtividade.c
@@ -14,7 +14,24 @@ double divisao(char* a, char* b){
 
 
 int main(){
+    double a, b;
+    char n, m;
 
+    printf("\nMULTIPLICACAO: ");
+    printf("\nDigite um numero: ");
+    scanf("%lf", &a);
+    printf("\nDigite outro numero: ");
+    scanf("%lf", &b);
+    printf("\n%.2f x %.2f = %.2f", a, b, multiplicacao(a, b));
+
+/*
+    printf("\nDIVISAO: ");
+    printf("\nDigite um numero: ");
+    scanf("%c");
+    printf("\nDigite outro numero: ");
+    scanf("%lf");
+    printf("\n%.2f / %.2f = %.2f ");
+*/
 
 
     return 0;
